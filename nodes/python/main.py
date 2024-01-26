5ed8357e66ef3296f2d92dea91376913256f6f6f
ValasekMaros <108756117+ValasekMaros@users.noreply.github.com>
2024-01-26 15:40:10 +0100

    Update main.py

--- nodes/python/main.py
+++ nodes/python/main.py
@@ -3,9 +3,8 @@ import time
 
 led = machine.Pin(2, machine.Pin.OUT)
 
-for i in range(8):
+for i in range(4):
     led.on()
-    time.sleep(0.5)
+    time.sleep(1)
     led.off()
-    time.sleep(0.5)
-machine.reset()
+    time.sleep(1)
