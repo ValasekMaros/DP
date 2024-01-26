4cb991697e172d74248905a9a31c8bacee3c9d8c
ValasekMaros <108756117+ValasekMaros@users.noreply.github.com>
2024-01-26 13:34:17 +0100

    Update boot.py

--- nodes/python/boot.py
+++ nodes/python/boot.py
@@ -4,5 +4,6 @@ from auth import SSID_Name, SSID_Pass
 firmware_url = "https://raw.githubusercontent.com/ValasekMaros/DP/main/nodes/python/"
 
 ota_updater = OTAUpdater(SSID_Name, SSID_Pass, firmware_url, "boot.py")
+ota_updater = OTAUpdater(SSID_Name, SSID_Pass, firmware_url, "main.py")
 
 ota_updater.download_and_install_update_if_available()
