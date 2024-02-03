befa0b4767e2c3edbbf47a8909ce98e62b6e297f
ValasekMaros <108756117+ValasekMaros@users.noreply.github.com>
2024-02-03 12:01:49 +0100

    Add files via upload

--- nodes/python/boot.py
+++ nodes/python/boot.py
@@ -12,6 +12,7 @@ zaciatok = time.time()
 sta_if = network.WLAN(network.STA_IF)
 
 sta_if.active(True)
+print('Wifi activated')
 sta_if.connect(auth.SSID_Name, auth.SSID_Pass)
 
 while not sta_if.isconnected():
@@ -24,4 +25,5 @@ firmware_url = "https://raw.githubusercontent.com/ValasekMaros/DP/main/nodes/pyt
 ota_updater = OTAUpdater(firmware_url, "main.py")
 
 ota_updater.download_and_install_update_if_available()
+
 print('...boot...')
