00c84455a78f6e4f7a7c4bca640908a67f8e8b72
ValasekMaros <108756117+ValasekMaros@users.noreply.github.com>
2024-03-08 14:48:54 +0100

    Create test_dp.sh

---
+++ server/mosquitto/test_dp.sh
@@ -0,0 +1,51 @@
+#! usr/bin/bash
+read -p "Number of records: " N
+read -p "Sleep time in seconds: " SLEEP
+read -p "MQTT Topic" topic
+read -p "ID number (00): " id
+read -p "Temperature(HIGH): " TempH
+read -p "Temperature(LOW): " TempL
+read -p "Humidity(HIGH): " HumH
+read -p "Humidity(LOW): " HumL
+read -p "Pressure(HIGH): " PressH
+read -p "Pressure(LOW): " PressL
+read -p "Rain(HIGH): " RainH
+read -p "Rain(LOW): " RainL
+read -p "Wind Speed(HIGH): " WindSpeedH
+read -p "Wind Speed(LOW): " WindSpeedL
+read -p "Wind Direction(HIGH): " WindDirH
+read -p "Wind Direction(LOW): " WindDirL
+read -p "Battery Voltage(HIGH): " BattVolH
+read -p "Battery Voltage(LOW): " BattVolL
+DIFFT=$(($TempH-$TempL+1))
+DIFFH=$(($HumH-$HumL+1))
+DIFFP=$(($PressH-$PressL+1))
+DIFFR=$(($RainH-$RainL+1))
+DIFFWS=$(($WindSpeedH-$WindSpeedL+1))
+DIFFWD=$(($WindDirH-$WindDirL+1))
+DIFFBV=$(($BattVolH-$BattVolL+1))
+RANDOM1=$$
+RANDOM2=$$
+echo ""
+echo "Start Testing..."
+echo ""
+
+for((i=1;i<=$N;i++))
+do
+	echo "Record Number: $i"
+	TBME=$(($(($RANDOM1%$DIFFT))+$TempL))
+  TDHT=$(($(($RANDOM2%$DIFFT))+$TempL))
+	HBME=$(($(($RANDOM1%$DIFFH))+$HumL))
+	HDHT=$(($(($RANDOM2%$DIFFH))+$HumL))
+  P=$(($(($RANDOM1%$DIFFB))+$PressL))
+  R=$(($(($RANDOM1%$DIFFR))+$RainL))
+  WS=$(($(($RANDOM1%$DIFFWS))+$WindSpeedL))
+  WD=$(($(($RANDOM1%$DIFFWD))+$WindDirL))
+  BV=$(($(($RANDOM1%$DIFFBV))+$BattVolL))
+	mosquitto_pub -h 192.168.1.250 -u dp -P Valasek_Maros -t '"'$topic'"' -m {'"espID"':'"'$id'"'", "'"bme_temp"':$T", "'"bme_hum"':$H", "'"bme_press"':$P", "'"dht_temp"':$T", "'"dht_hum"':$H", "'"rain_tips"':$Random1", "'"rain_mm"':$R", "'"windSpeed_tips"':$Random1", "'"windSpeed_1Hz"':$Random2", "'"windSpeed_kmh"':($Random1+$Random2)/2", "'"windSpeed_ms"':$WS", "'"windDir_deg"':$WD", "'"windDir_Name"':"N"", "'"windDir_ADC"':$WD*100", "'"battery_voltage"':$BV}
+	sleep $SLEEP
+done
+
+echo ""
+echo "Test ended..."
+echo ""
