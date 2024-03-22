c3b5968fcfaf28a6fb5b167308c69fc96c2555c7
ValasekMaros <108756117+ValasekMaros@users.noreply.github.com>
2024-03-22 09:39:18 +0100

    Add files via upload

--- nodes/python/boot.py
+++ nodes/python/boot.py
@@ -5,7 +5,8 @@ from ota import OTAUpdater
 import time
 
 machine.freq(80000000)
-
+errorTime = 300
+calc_interval = 15000
 print('...boot...')
 zaciatok = time.time()
 
@@ -15,8 +16,19 @@ sta_if.active(True)
 print('Wifi activated')
 sta_if.connect(auth.SSID_Name, auth.SSID_Pass)
 
+nextcalc = round(time.time_ns() / 1000000) + calc_interval
 while not sta_if.isconnected():
-    pass
+    timer = round(time.time_ns() / 1000000)
+    if timer >= nextcalc:
+        print('Cant connect to WiFi, error')
+        endTime = time.time()
+        cycleTime = endTime - zaciatok
+        print('Cycle time:', cycleTime)
+        print('Error sleep')
+        machine.deepsleep((errorTime - cycleTime) * 1000)
+        machine.reset()
+
+
 print('Connection successful')
 print(sta_if.ifconfig())
         
