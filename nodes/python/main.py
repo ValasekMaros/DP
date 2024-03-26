f07940edeeec2ed98b1b709abd2dc77ace75b8ce
ValasekMaros <108756117+ValasekMaros@users.noreply.github.com>
2024-03-26 17:06:38 +0100

    Update main.py

--- nodes/python/main.py
+++ nodes/python/main.py
@@ -38,7 +38,7 @@ message = {
 # Sleep time(in seconds) for sleep after error and sleep after successful message send, and for warming sensors
 warmSensor = 5
 errorTime = 300
-sendTime = 3600
+sendTime = 1800
 # MQTT ID for connect
 #mqtt_client = ubinascii.hexlify(machine.unique_id())
 mqtt_client = "MeteoStation00"
